105b0370f274ae2e27891ff2cee32a94f1f1e0d7
Linas Nikiperavičius <linas@linasdev.com>
2021-07-18 20:43:32 +0300

    Remove `bootloader_start_upload_event`

--- src/ross_event/ross_bootloader_event.rs
+++ src/ross_event/ross_bootloader_event.rs
@@ -6,7 +6,6 @@ use crate::ross_event::ross_event_packet::RossEventPacketError;
 use crate::ross_packet::RossPacket;
 
 pub const ROSS_BOOTLOADER_HELLO_EVENT_CODE: u16 = 0x0000;
-pub const ROSS_BOOTLOADER_START_UPLOAD_EVENT_CODE: u16 = 0x0001;
 
 #[derive(Debug, PartialEq)]
 pub struct RossBootloaderHelloEvent {
@@ -56,58 +55,6 @@ impl RossConvertPacket<RossBootloaderHelloEvent> for RossBootloaderHelloEvent {
         }
 
         for byte in u32::to_be_bytes(self.firmware_version).iter() {
-            data.push(*byte);
-        }
-
-        RossPacket {
-            is_error: false,
-            device_address: self.device_address,
-            data,
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct RossBootloaderStartUploadEvent {
-    pub device_address: u16,
-    pub programmer_address: u16,
-}
-
-impl RossConvertPacket<RossBootloaderStartUploadEvent> for RossBootloaderStartUploadEvent {
-    fn try_from_packet(packet: RossPacket) -> Result<Self, RossConvertPacketError> {
-        if packet.data.len() != 4 {
-            return Err(RossConvertPacketError::WrongSize);
-        }
-
-        if packet.is_error {
-            return Err(RossConvertPacketError::WrongType);
-        }
-
-        if u16::from_be_bytes(packet.data[0..=1].try_into().unwrap())
-            != ROSS_BOOTLOADER_START_UPLOAD_EVENT_CODE
-        {
-            return Err(RossConvertPacketError::EventPacket(
-                RossEventPacketError::WrongEventType,
-            ));
-        }
-
-        let device_address = packet.device_address;
-        let programmer_address = u16::from_be_bytes(packet.data[2..=3].try_into().unwrap());
-
-        Ok(RossBootloaderStartUploadEvent {
-            device_address,
-            programmer_address,
-        })
-    }
-
-    fn to_packet(&self) -> RossPacket {
-        let mut data = vec!();
-
-        for byte in u16::to_be_bytes(ROSS_BOOTLOADER_START_UPLOAD_EVENT_CODE).iter() {
-            data.push(*byte);
-        }
-
-        for byte in u16::to_be_bytes(self.programmer_address).iter() {
             data.push(*byte);
         }
 
--- src/tests/ross_event_test/ross_bootloader_event_test.rs
+++ src/tests/ross_event_test/ross_bootloader_event_test.rs
@@ -87,7 +87,7 @@ fn try_from_packet_bootloader_hello_event_wrong_event_type_test() {
 
 #[test]
 fn to_packet_bootloader_hello_event_test() {
-    let programmer_start_upload_event = RossBootloaderHelloEvent {
+    let bootloader_hello_event = RossBootloaderHelloEvent {
         device_address: 0xabab,
         programmer_address: 0x0123,
         firmware_version: 0x01234567,
@@ -105,41 +105,5 @@ fn to_packet_bootloader_hello_event_test() {
         0x67, // firmware_version
     ];
 
-    assert_eq!(programmer_start_upload_event.to_packet(), packet);
-}
-
-
-#[test]
-fn try_from_packet_bootloader_start_upload_event_test() {
-    let mut packet = EVENT_PACKET;
-    packet.data = vec![
-        0x00, // event code
-        0x01, // event code
-        0x01, // programmer_address
-        0x23, // programmer_address
-    ];
-
-    let bootloader_start_upload_event =
-        RossBootloaderStartUploadEvent::try_from_packet(packet).unwrap();
-
-    assert_eq!(bootloader_start_upload_event.device_address, 0xabab);
-    assert_eq!(bootloader_start_upload_event.programmer_address, 0x0123);
-}
-
-#[test]
-fn to_packet_bootloader_start_upload_event_test() {
-    let bootloader_start_upload_event = RossBootloaderStartUploadEvent {
-        device_address: 0xabab,
-        programmer_address: 0x0123,
-    };
-
-    let mut packet = EVENT_PACKET;
-    packet.data = vec![
-        0x00, // event code
-        0x01, // event code
-        0x01, // programmer_address
-        0x23, // programmer_address
-    ];
-
-    assert_eq!(bootloader_start_upload_event.to_packet(), packet);
+    assert_eq!(bootloader_hello_event.to_packet(), packet);
 }
