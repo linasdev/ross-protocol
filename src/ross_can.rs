761e6ae150abc389daf30914bb31f3d72164e77a
Linas Nikiperavičius <linas@linasdev.com>
2021-07-18 14:53:36 +0300

    Fix RossCan::try_get_packet

--- src/ross_can.rs
+++ src/ross_can.rs
@@ -27,36 +27,38 @@ impl<I: Instance> RossCan<I> {
     }
 
     pub fn try_get_packet(&mut self) -> Result<RossPacket, RossCanError> {
-        match self.can.receive() {
-            Ok(frame) => {
-                let ross_frame = match RossFrame::from_bxcan_frame(frame) {
-                    Ok(frame) => frame,
-                    Err(err) => return Err(RossCanError::FrameError(err)),
-                };
-
-                if let Some(ref mut packet_builder) = self.packet_builder {
-                    if let Err(err) = packet_builder.add_frame(ross_frame) {
-                        return Err(RossCanError::BuilderError(err));
-                    }
-                } else {
-                    self.packet_builder = match RossPacketBuilder::new(ross_frame) {
-                        Ok(builder) => Some(builder),
-                        Err(err) => return Err(RossCanError::BuilderError(err)),
+        loop {
+            match self.can.receive() {
+                Ok(frame) => {
+                    let ross_frame = match RossFrame::from_bxcan_frame(frame) {
+                        Ok(frame) => frame,
+                        Err(err) => return Err(RossCanError::FrameError(err)),
                     };
-                }
 
-                if let Some(ref mut packet_builder) = self.packet_builder {
-                    if packet_builder.frames_left() == 0 {
-                        let packet = match packet_builder.build() {
-                            Ok(packet) => packet,
+                    if let Some(ref mut packet_builder) = self.packet_builder {
+                        if let Err(err) = packet_builder.add_frame(ross_frame) {
+                            return Err(RossCanError::BuilderError(err));
+                        }
+                    } else {
+                        self.packet_builder = match RossPacketBuilder::new(ross_frame) {
+                            Ok(builder) => Some(builder),
                             Err(err) => return Err(RossCanError::BuilderError(err)),
                         };
-
-                        return Ok(packet);
                     }
-                }
-            },
-            Err(_) => return Err(RossCanError::BufferOverrun),
+
+                    if let Some(ref mut packet_builder) = self.packet_builder {
+                        if packet_builder.frames_left() == 0 {
+                            let packet = match packet_builder.build() {
+                                Ok(packet) => packet,
+                                Err(err) => return Err(RossCanError::BuilderError(err)),
+                            };
+
+                            return Ok(packet);
+                        }
+                    }
+                },
+                Err(_) => break,
+            }
         }
 
         Err(RossCanError::NoPacketReceived)
