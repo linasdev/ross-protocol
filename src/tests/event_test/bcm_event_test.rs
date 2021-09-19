fe1bc0bb646acb7abdebd67b7e5756448ae8d0b6
Linas Nikiperavičius <linas@linasdev.com>
2021-09-19 21:50:15 +0300

    Fix some tests

--- src/tests/event_test/bcm_event_test.rs
+++ src/tests/event_test/bcm_event_test.rs
@@ -18,31 +18,37 @@ fn try_from_packet_bcm_change_brightness_event_test() {
     packet.data = vec![
         ((BCM_CHANGE_BRIGHTNESS_EVENT_CODE >> 8) & 0xff) as u8, // event code
         ((BCM_CHANGE_BRIGHTNESS_EVENT_CODE >> 0) & 0xff) as u8, // event code
-        0x01,                                                   // channel
-        0x23,                                                   // brightness
+        0x01,                                                   // transmitter_address
+        0x23,                                                   // transmitter_address
+        0x45,                                                   // channel
+        0x67,                                                   // brightness
     ];
 
     let bcm_change_brightness_event = BcmChangeBrightnessEvent::try_from_packet(&packet).unwrap();
 
     assert_eq!(bcm_change_brightness_event.bcm_address, 0xabab);
-    assert_eq!(bcm_change_brightness_event.channel, 0x01);
-    assert_eq!(bcm_change_brightness_event.brightness, 0x23);
+    assert_eq!(bcm_change_brightness_event.transmitter_address, 0x0123);
+    assert_eq!(bcm_change_brightness_event.channel, 0x45);
+    assert_eq!(bcm_change_brightness_event.brightness, 0x67);
 }
 
 #[test]
 fn to_packet_bcm_change_brightness_event_test() {
     let bootloader_hello_event = BcmChangeBrightnessEvent {
         bcm_address: 0xabab,
-        channel: 0x01,
-        brightness: 0x23,
+        transmitter_address: 0x0123,
+        channel: 0x45,
+        brightness: 0x67,
     };
 
     let mut packet = EVENT_PACKET;
     packet.data = vec![
         ((BCM_CHANGE_BRIGHTNESS_EVENT_CODE >> 8) & 0xff) as u8, // event code
         ((BCM_CHANGE_BRIGHTNESS_EVENT_CODE >> 0) & 0xff) as u8, // event code
-        0x01,                                                   // channel
-        0x23,                                                   // brightness
+        0x01,                                                   // transmitter_address
+        0x23,                                                   // transmitter_address
+        0x45,                                                   // channel
+        0x67,                                                   // brightness
     ];
 
     assert_eq!(bootloader_hello_event.to_packet(), packet);
