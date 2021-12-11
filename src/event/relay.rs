9b70476f5c6eb0bff7f878abc26a1f69526af3ca
Linas Nikiperavičius <linas@linasdev.com>
2021-12-11 16:17:59 +0200

    Remove relay events and add relay set value event

--- src/event/relay.rs
+++ src/event/relay.rs
@@ -1,22 +1,38 @@
 use alloc::vec;
 use core::convert::TryInto;
+use core::mem::{transmute_copy, size_of};
 
 use crate::convert_packet::{ConvertPacket, ConvertPacketError};
 use crate::event::event_code::*;
 use crate::event::EventError;
 use crate::packet::Packet;
 
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum RelayValue {
+    Single(bool),
+    DoubleExclusive(RelayDoubleExclusiveValue),
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub enum RelayDoubleExclusiveValue {
+    FirstChannelOn,
+    SecondChannelOn,
+    NoChannelOn,
+}
+
 #[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub struct RelaySetStateEvent {
+pub struct RelaySetValueEvent {
     pub relay_address: u16,
     pub transmitter_address: u16,
     pub index: u8,
-    pub state: bool,
+    pub value: RelayValue,
 }
 
-impl ConvertPacket<RelaySetStateEvent> for RelaySetStateEvent {
+impl ConvertPacket<RelaySetValueEvent> for RelaySetValueEvent {
     fn try_from_packet(packet: &Packet) -> Result<Self, ConvertPacketError> {
-        if packet.data.len() != 6 {
+        if packet.data.len() != 5 + size_of::<RelayValue>() {
             return Err(ConvertPacketError::WrongSize);
         }
 
@@ -24,7 +40,7 @@ impl ConvertPacket<RelaySetStateEvent> for RelaySetStateEvent {
             return Err(ConvertPacketError::WrongType);
         }
 
-        if u16::from_be_bytes(packet.data[0..=1].try_into().unwrap()) != RELAY_SET_STATE_EVENT_CODE
+        if u16::from_be_bytes(packet.data[0..=1].try_into().unwrap()) != RELAY_SET_VALUE_EVENT_CODE
         {
             return Err(ConvertPacketError::Event(EventError::WrongEventType));
         }
@@ -32,20 +48,26 @@ impl ConvertPacket<RelaySetStateEvent> for RelaySetStateEvent {
         let relay_address = packet.device_address;
         let transmitter_address = u16::from_be_bytes(packet.data[2..=3].try_into().unwrap());
         let index = packet.data[4];
-        let state = packet.data[5] != 0x00;
+        let value = unsafe {
+            transmute_copy::<[u8; size_of::<RelayValue>()], RelayValue>(
+                &packet.data[5..5 + size_of::<RelayValue>()]
+                    .try_into()
+                    .unwrap(),
+            )
+        };
 
         Ok(Self {
             relay_address,
             transmitter_address,
             index,
-            state,
+            value,
         })
     }
 
     fn to_packet(&self) -> Packet {
         let mut data = vec![];
 
-        for byte in u16::to_be_bytes(RELAY_SET_STATE_EVENT_CODE).iter() {
+        for byte in u16::to_be_bytes(RELAY_SET_VALUE_EVENT_CODE).iter() {
             data.push(*byte);
         }
 
@@ -54,60 +76,13 @@ impl ConvertPacket<RelaySetStateEvent> for RelaySetStateEvent {
         }
 
         data.push(self.index);
-        data.push(if self.state { 0x01 } else { 0x00 });
 
-        Packet {
-            is_error: false,
-            device_address: self.relay_address,
-            data,
+        unsafe {
+            for byte in transmute_copy::<RelayValue, [u8; size_of::<RelayValue>()]>(&self.value).iter()
+            {
+                data.push(*byte);
+            }
         }
-    }
-}
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
-pub struct RelayFlipStateEvent {
-    pub relay_address: u16,
-    pub transmitter_address: u16,
-    pub index: u8,
-}
-
-impl ConvertPacket<RelayFlipStateEvent> for RelayFlipStateEvent {
-    fn try_from_packet(packet: &Packet) -> Result<Self, ConvertPacketError> {
-        if packet.data.len() != 5 {
-            return Err(ConvertPacketError::WrongSize);
-        }
-
-        if packet.is_error {
-            return Err(ConvertPacketError::WrongType);
-        }
-
-        if u16::from_be_bytes(packet.data[0..=1].try_into().unwrap()) != RELAY_FLIP_STATE_EVENT_CODE
-        {
-            return Err(ConvertPacketError::Event(EventError::WrongEventType));
-        }
-
-        let relay_address = packet.device_address;
-        let transmitter_address = u16::from_be_bytes(packet.data[2..=3].try_into().unwrap());
-        let index = packet.data[4];
-
-        Ok(Self {
-            relay_address,
-            transmitter_address,
-            index,
-        })
-    }
-
-    fn to_packet(&self) -> Packet {
-        let mut data = vec![];
-
-        for byte in u16::to_be_bytes(RELAY_FLIP_STATE_EVENT_CODE).iter() {
-            data.push(*byte);
-        }
-
-        for byte in u16::to_be_bytes(self.transmitter_address).iter() {
-            data.push(*byte);
-        }
-
-        data.push(self.index);
 
         Packet {
             is_error: false,
@@ -131,77 +106,53 @@ mod tests {
     fn set_state_try_from_packet_test() {
         let mut packet = EVENT_PACKET;
         packet.data = vec![
-            ((RELAY_SET_STATE_EVENT_CODE >> 8) & 0xff) as u8, // event code
-            ((RELAY_SET_STATE_EVENT_CODE >> 0) & 0xff) as u8, // event code
+            ((RELAY_SET_VALUE_EVENT_CODE >> 8) & 0xff) as u8, // event code
+            ((RELAY_SET_VALUE_EVENT_CODE >> 0) & 0xff) as u8, // event code
             0x01,                                             // transmitter address
             0x23,                                             // transmitter address
             0x45,                                             // index
-            0x01,                                             // state
+            0x01,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x01,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
         ];
 
-        let event = RelaySetStateEvent::try_from_packet(&packet).unwrap();
+        let event = RelaySetValueEvent::try_from_packet(&packet).unwrap();
 
         assert_eq!(event.relay_address, 0xabab);
         assert_eq!(event.transmitter_address, 0x0123);
         assert_eq!(event.index, 0x45);
-        assert_eq!(event.state, true);
+        assert_eq!(event.value, RelayValue::DoubleExclusive(RelayDoubleExclusiveValue::SecondChannelOn));
     }
 
     #[test]
     fn set_state_to_packet_test() {
-        let event = RelaySetStateEvent {
+        let event = RelaySetValueEvent {
             relay_address: 0xabab,
             transmitter_address: 0x0123,
             index: 0x45,
-            state: true,
+            value: RelayValue::DoubleExclusive(RelayDoubleExclusiveValue::SecondChannelOn),
         };
 
         let mut packet = EVENT_PACKET;
         packet.data = vec![
-            ((RELAY_SET_STATE_EVENT_CODE >> 8) & 0xff) as u8, // event code
-            ((RELAY_SET_STATE_EVENT_CODE >> 0) & 0xff) as u8, // event code
+            ((RELAY_SET_VALUE_EVENT_CODE >> 8) & 0xff) as u8, // event code
+            ((RELAY_SET_VALUE_EVENT_CODE >> 0) & 0xff) as u8, // event code
             0x01,                                             // transmitter address
             0x23,                                             // transmitter address
             0x45,                                             // index
-            0x01,                                             // state
-        ];
-
-        assert_eq!(event.to_packet(), packet);
-    }
-
-    #[test]
-    fn flip_state_try_from_packet_test() {
-        let mut packet = EVENT_PACKET;
-        packet.data = vec![
-            ((RELAY_FLIP_STATE_EVENT_CODE >> 8) & 0xff) as u8, // event code
-            ((RELAY_FLIP_STATE_EVENT_CODE >> 0) & 0xff) as u8, // event code
-            0x01,                                              // transmitter address
-            0x23,                                              // transmitter address
-            0x45,                                              // index
-        ];
-
-        let event = RelayFlipStateEvent::try_from_packet(&packet).unwrap();
-
-        assert_eq!(event.relay_address, 0xabab);
-        assert_eq!(event.transmitter_address, 0x0123);
-        assert_eq!(event.index, 0x45);
-    }
-
-    #[test]
-    fn flip_state_to_packet_test() {
-        let event = RelayFlipStateEvent {
-            relay_address: 0xabab,
-            transmitter_address: 0x0123,
-            index: 0x45,
-        };
-
-        let mut packet = EVENT_PACKET;
-        packet.data = vec![
-            ((RELAY_FLIP_STATE_EVENT_CODE >> 8) & 0xff) as u8, // event code
-            ((RELAY_FLIP_STATE_EVENT_CODE >> 0) & 0xff) as u8, // event code
-            0x01,                                              // transmitter address
-            0x23,                                              // transmitter address
-            0x45,                                              // index
+            0x01,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x01,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
+            0x00,                                             // value
         ];
 
         assert_eq!(event.to_packet(), packet);
